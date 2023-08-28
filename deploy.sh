243955190d9f66c780f9be613b326365162823ae
CharlyArm <cramirezruiz30@gmail.com>
2023-08-28 16:03:53 +0000

    Codificacion deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-# Ejecutar el playbook de Ansible
-ansible-playbook ansible/update_upgrade.yml
+# Ejecutar el playbook de Ansible y mostrar mensaje de éxito
+ansible-playbook ansible/update_upgrade.yml && cat << "EOF"
+
+                     ,----,          ,--,    __  ,-.
+                   .'   .`|        ,'_ /|  ,' ,'/ /|
+   ,--.--.      .'   .'  .'   .--. |  | :  '  | |' |    ,--.--.
+  /       \   ,---, '   ./  ,'_ /| :  . |  |  |   ,'   /       \
+ .--.  .-. |  ;   | .'  /   |  ' | |  . .  '  :  /    .--.  .-. |
+  \__\/: . .  `---' /  ;--, |  | ' |  | |  |  | '      \__\/: . .
+  ," .--.; |    /  /  / .`| :  | : ;  ; |  ;  : |      ," .--.; |
+ /  /  ,.  |  ./__;     .'  '  :  `--'   \ |  , ;     /  /  ,.  |
+;  :   .'   \ ;   |  .'     :  ,      .-./  ---'     ;  :   .'   \
+|  ,     .-./ `---'          `--`----'               |  ,     .-./
+ `--`---'                                             `--`---'
+		The Software Contact Center Solution
+           Copyright (C) 2023 https://cognifirefly.com.co/
+
+#############################################################
+#         azura installation ended successfully         #
+#############################################################
+
+EOF
