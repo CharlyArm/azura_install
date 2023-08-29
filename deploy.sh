e2c0c822c150c3267fe3d2de4244ec76519d32fd
CharlyArm <cramirezruiz30@gmail.com>
2023-08-29 23:20:37 +0000

    Codificacion deploy

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# Ejecutar el playbook de Ansible y mostrar mensaje de éxito
-ansible-playbook ansible/update_upgrade.yml &&
+# Ejecutar el playbook de Ansible para instalar addons
+ansible-playbook components/addons/playbook.yml &&
 
-# Ejecutar el playbook de Ansible para instalar Nginx
-ansible-playbook ansible/install_nginx.yml &&
+# Ejecutar el playbook de Ansible para instalar nginx
+ansible-playbook components/nginx/playbook.yml &&
 
-# Ejecutar el playbook de Ansible para instalar PHP Composer Laravel
-ansible-playbook ansible/install_php_composer_laravel.yml &&
+# Ejecutar el playbook de Ansible para instalar php
+ansible-playbook components/php/playbook.yml &&
+
+# Ejecutar el playbook de Ansible para instalar composer
+ansible-playbook components/composer/playbook.yml &&
+
+# Ejecutar el playbook de Ansible para instalar laravel
+ansible-playbook components/laravel/playbook.yml &&
 
 # Ejecutar el playbook de Ansible para instalar MariaDB
-ansible-playbook ansible/install_mariadb.yml &&
+ansible-playbook components/mariadb/playbook.yml &&
 
 # Mostrar mensaje de éxito
 cat << "EOF"
